e3dbead96a3c2d8e381b406e27ec8657d067fbc8
Iván Topp Sandoval <itopp2015@alu.uct.cl>
2021-02-12 20:28:19 -0300

    [ADD] Get public designs by user [MOD] Return can fetch more designs boolean when request designs

--- src/controllers/design.js
+++ src/controllers/design.js
@@ -4,12 +4,13 @@ const User = require('../models/User');
 const Folder = require('../models/Folder');
 const Design = require('../models/Design');
 const { successResponse, badRequest, internalServerError, createdSuccessful, unauthorized } = require('../utils/responses');
+const mongoose = require('mongoose');
 
-const getRecentDesigns = async (req, res = response)=>{ 
+const getRecentDesigns = async (req, res = response) => {
     const { uid } = req;
     try {
         const designs = await Design.find({ privileges: { $elemMatch: { user: uid, type: 0 } } })
-            .sort({updatedOn:-1})
+            .sort({ updatedOn: -1 })
             .limit(5)
             .populate('metadata.category')
             .populate('owner', 'name lastname');
@@ -20,39 +21,52 @@ const getRecentDesigns = async (req, res = response)=>{
     }
 }
 
-const getUserDesignsAndForldersByPath = async (req, res = response)=>{
+const getUserDesignsAndFoldersByPath = async (req, res = response) => {
     const { uid } = req;
     const { path, from, limit } = req.body;
-    if(!uid) return badRequest('No se ha especificado un usuario.', res);
-    if(!path) return badRequest('No se ha especificado un carpeta.', res);
+    if (!uid) return badRequest('No se ha especificado un usuario.', res);
+    if (!path) return badRequest('No se ha especificado un carpeta.', res);
     try {
         const folder = await Folder.findOne({ path, owner: uid });
         const folders = await Folder.find({ parent: folder.id, owner: uid });
+        let canFetchMore = false;
+        await Design.aggregate([
+            {
+                '$match': {
+                    "owner": { '$eq': mongoose.Types.ObjectId(uid) },
+                    "folder": { '$eq': mongoose.Types.ObjectId(folder.id) }
+                }
+            },
+            { '$count': "q" }
+        ], function (err, result) {
+            if( err ) return internalServerError('Porfavor hable con el administrador.', res);
+            if( result.length > 0 ) canFetchMore = result[0].q > (limit || 12);
+        });
         const designs = await Design.find({ owner: uid, folder: folder.id })
             .skip(from || 0)
             .limit(limit || 12)
             .populate('metadata.category')
             .populate('owner', 'name lastname')
             .populate('folder', 'owner path parent');
-        return successResponse('Diseños obtenidos con éxito.', { folders, designs }, res);
+        return successResponse('Diseños obtenidos con éxito.', { ownerId: uid, canFetchMore, folders, designs }, res);
     } catch (error) {
         console.log(error);
         return internalServerError('Porfavor hable con el administrador.', res);
     }
 }
 
-const getDesignsSharedWithUser = async (req, res = response)=>{ 
+const getDesignsSharedWithUser = async (req, res = response) => {
     return successResponse('getDesignsSharedWithUser: Connected!!', ['Design#1', 'Design#2', 'Design#3'], res);
 }
 
 const deleteDesign = async (req, res = response) => {
     const { uid } = req;
     const id = req.params.id;
-    if(!id) return badRequest('No se ha especificado un diseño.', res);
+    if (!id) return badRequest('No se ha especificado un diseño.', res);
     try {
-        const design = await Design.findById( id );
-        if(design.owner.toString() !== uid) return unauthorized('Usted no está autorizado para eliminar este diseño.', res);
-        const deleted = await Design.findByIdAndDelete( id );
+        const design = await Design.findById(id);
+        if (design.owner.toString() !== uid) return unauthorized('Usted no está autorizado para eliminar este diseño.', res);
+        const deleted = await Design.findByIdAndDelete(id);
         return successResponse('Se ha eliminado el diseño con éxito.', deleted, res);
     } catch (error) {
         console.log(error);
@@ -60,9 +74,43 @@ const deleteDesign = async (req, res = response) => {
     }
 }
 
+const getPublicDesignsByUser = async (req, res = response) => {
+    const { id, from, limit } = req.body;
+    if (!id) return badRequest('No se ha especificado un usuario.', res);
+    if (!mongoose.Types.ObjectId.isValid(id)) return badRequest('No existe usuario con la id especificada.', res);
+    try {
+        const user = User.findById(id);
+        if (!user) return badRequest('No existe usuario con la id especificada.', res);
+        let canFetchMore = false;
+        await Design.aggregate([
+            {
+                '$match': {
+                    "owner": { '$eq': mongoose.Types.ObjectId(id) },
+                    "metadata.public": { '$eq': true }
+                }
+            },
+            { '$count': "q" }
+        ], function (err, result) {
+            if( err ) return internalServerError('Porfavor hable con el administrador.', res);
+            if( result.length > 0 ) canFetchMore = result[0].q > (limit || 12);
+        });
+        const designs = await Design.find({ owner: id, 'metadata.public': true })
+            .skip(from || 0)
+            .limit(limit || 12)
+            .populate('metadata.category')
+            .populate('owner', 'name lastname')
+            .populate('folder', 'owner path parent');
+        return successResponse('Se han obtenido con éxito los diseños públicos del usuario especificado.', { ownerId: id, canFetchMore, designs }, res);
+    } catch (error) {
+        console.log(error);
+        return internalServerError('Porfavor hable con el administrador.', res);
+    }
+};
+
 module.exports = {
     getRecentDesigns,
-    getUserDesignsAndForldersByPath,
+    getUserDesignsAndFoldersByPath,
     getDesignsSharedWithUser,
     deleteDesign,
+    getPublicDesignsByUser,
 };
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,12 +1,13 @@
 const {response} = require('express');
+const mongoose = require('mongoose');
 const User = require('../models/User');
 const { badRequest, internalServerError, successResponse } = require('../utils/responses');
 
 const getUserProfile = async(req, res = response)=>{
     const uid = req.params.uid;
     try {
+        if (!mongoose.Types.ObjectId.isValid(uid)) return badRequest('No existe usuario con la id especificada.', res);
         const user = await User.findById( uid );
-        console.log('Usuario '+ user);
         if(!user) return badRequest('Usuario no encontrado.', res); 
         return successResponse('Usuario encontrado.', user, res);
     } catch (error) {
@@ -18,7 +19,6 @@ const searchOtherUserProfile = async(req, res = response)=>{
     const { name } = req.body;
     try {
         const user = await User.findOne( {name:name} );
-        console.log('Usuario '+ user);
         if(!user) return badRequest('Usuario no encontrado.', res); 
         return successResponse('Usuario encontrado.', { user:{
             uid: user.id,
