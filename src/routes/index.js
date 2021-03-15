eda773ea0288dc5c783555300b8cca20de46b4b3
Iván Topp Sandoval <itopp2015@alu.uct.cl>
2021-03-15 13:15:12 -0300

    [FIX] Redirect to react router when a get route don't exists

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use('/api/auth', require('./auth'));
 app.use('/api/design', require('./design'));
@@ -9,7 +10,7 @@ app.use('/api/category', require('./category'));
 app.use('/api/bloom', require('./bloom'));
 
 app.get('*', (req, res) =>{
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(path.resolve(__dirname, '../../public/index.html')));
 });
 
 module.exports = app;
